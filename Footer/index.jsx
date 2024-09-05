47cd82addc64b9f01f06068ae2d7c4657cfe0cb4
Ian Vincent L. Reyes <99298221+IanReyes2@users.noreply.github.com>
2024-09-06 00:48:04 +0800

    Add files via upload

---
+++ Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Au Revior!</div>
+  )
+}
+
+export default Footer
---
+++ HeroSection/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>Bonjour!</div>
+  )
+}
+
+export default HeroSection
